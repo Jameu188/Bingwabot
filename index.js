29922411da8b911e40a000b3cc8e8f0522f71244
Jameu188 <140157021+Jameu188@users.noreply.github.com>
2026-02-20 04:53:55 +0300

    Change TELEGRAM_TOKEN to a new value
    Updated TELEGRAM_TOKEN for security reasons.

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ process.on("uncaughtException", (err) => console.log("uncaughtException:", err?.
 
 // ===================== CONFIG =====================
 // ⚠️ IMPORTANT: Move secrets to Render Environment Variables (recommended)
-const TELEGRAM_TOKEN = "8179985214:AAHnEgYbgq33KJrOpGv5RTZcuyhFaCKJdyc";
+const TELEGRAM_TOKEN = "8179985214:AAEaYg8hZXlI4w-nS0_gjItCYDsJsXz0eig";
 const PAYHERO_USERNAME = "UuT8gpaqwB5ttjYC4ivd";
 const PAYHERO_PASSWORD = "FIfH59osWhh2cwwrsWfxtnx8K7SPjhitehpPgAmZ";
 // ✅ ADMIN
