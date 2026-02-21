0a9881d09bf8075f852bb9dfca44614deb7fcd71
Jameu188 <140157021+Jameu188@users.noreply.github.com>
2026-02-21 05:24:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ process.on("uncaughtException", (err) => console.log("uncaughtException:", err?.
 
 // ===================== CONFIG =====================
 // ⚠️ IMPORTANT: Move secrets to Render Environment Variables (recommended)
-const TELEGRAM_TOKEN = "8179985214:AAHnEgYbgq33KJrOpGv5RTZcuyhFaCKJdyc";
+const TELEGRAM_TOKEN = "8179985214:AAHnf2wGQ4aXqqZUX02u_0rLYGronNhwP8Y";
 const PAYHERO_USERNAME = "UuT8gpaqwB5ttjYC4ivd";
 const PAYHERO_PASSWORD = "FIfH59osWhh2cwwrsWfxtnx8K7SPjhitehpPgAmZ";
 // ✅ ADMIN
